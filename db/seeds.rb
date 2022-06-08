8471e4db580d154dbfb9e91f1b136cd5ed6468d2
Alex Vlasov <drumlife182@gmail.com>
2022-06-08 20:04:46 +0200

    Integrated index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,8 +24,8 @@ puts '5 skills created'
       title: "Portfolio title #{portfolio_item}",
       subtitle: 'Ruby on Rails',
       body: 'RubyGems.org is made possible through a partnership with the greater Ruby community. Fastly provides bandwidth and CDN support, Ruby Central covers infrastructure costs, and Ruby Together funds ongoing development and ops work. Learn more about our sponsors and how they work together.',
-      main_image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngaaa.com%2Fdetail%2F997979&psig=AOvVaw2oo72vheL1YiGRZnZB7qq7&ust=1654258645041000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCLjf2-vfjvgCFQAAAAAdAAAAABAD',
-      thumb_image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngaaa.com%2Fdetail%2F997979&psig=AOvVaw2oo72vheL1YiGRZnZB7qq7&ust=1654258645041000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCLjf2-vfjvgCFQAAAAAdAAAAABAD'
+      main_image: 'https://place-hold.it/600x400.jpg/888/fff/',
+      thumb_image: 'https://place-hold.it/300x200.jpg/888/fff/'
   )
 end
 
