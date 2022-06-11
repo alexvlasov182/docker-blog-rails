858eda0b091302ee270b99c9a95e7271b34ce23e
Alex Vlasov <drumlife182@gmail.com>
2022-06-11 17:27:57 +0200

    Implemented custom scopes for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,11 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :subtitle, :body
+
+  def self.angular
+    where(subtitle: 'Angular')
+  end
+
+  scope :ruby_on_rails, -> {where(subtitle: "Ruby on Rails")}
 end
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,26 @@
+3.times do |topic|
+  Topic.create!(
+     title: "Topic #{topic}"
+  )
+end
+
 10.times do |blog|
   Blog.create!(
-      title: "My Blog Post About Ruby on Rails #{blog}",
-      body: "RubyGems.org is the Ruby community’s gem hosting service. Instantly publish your gems and then install them. Use the API to find out more about available gems. Become a contributor and improve the site yourself.
+    title: "My Blog Post About Ruby on Rails #{blog}",
+    body: "RubyGems.org is the Ruby community’s gem hosting service. Instantly publish your gems and then install them. Use the API to find out more about available gems. Become a contributor and improve the site yourself.
 RubyGems.org is made possible through a partnership with the greater Ruby community. Fastly provides bandwidth and CDN support, Ruby Central covers infrastructure costs, and Ruby Together funds ongoing development and ops work. Learn more about our sponsors and how they work together.
 We need your help to fund the developer time that keeps RubyGems.org running smoothly for everyone. Join Ruby Together today.
-
-")
+",
+    topic_id: Topic.last.id
+    )
 end
 
 puts '10 blog posts created'
 
 5.times do |skill|
   Skill.create!(
-      title: "Rails #{skill}",
-      percent_utilized: 15
+    title: "Rails #{skill}",
+    percent_utilized: 15
   )
 end
 
@@ -21,9 +28,19 @@ puts '5 skills created'
 
 8.times do |portfolio_item|
   Portfolio.create!(
+    title: "Portfolio title #{portfolio_item}",
+    subtitle: 'Ruby on Rails',
+    body: 'RubyGems.org is made possible through a partnership with the greater Ruby community. Fastly provides bandwidth and CDN support, Ruby Central covers infrastructure costs, and Ruby Together funds ongoing development and ops work. Learn more about our sponsors and how they work together.',
+    main_image: 'https://place-hold.it/600x400.jpg/888/fff/',
+    thumb_image: 'https://place-hold.it/300x200.jpg/888/fff/'
+  )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
       title: "Portfolio title #{portfolio_item}",
-      subtitle: 'Ruby on Rails',
-      body: 'RubyGems.org is made possible through a partnership with the greater Ruby community. Fastly provides bandwidth and CDN support, Ruby Central covers infrastructure costs, and Ruby Together funds ongoing development and ops work. Learn more about our sponsors and how they work together.',
+      subtitle: 'Angular',
+      body: 'Build features quickly with simple, declarative templates. Extend the template language with your own components and use a wide array of existing components. Get immediate Angular-specific help and feedback with nearly every IDE and editor. All this comes together so you can focus on building amazing apps rather than trying to make the code work.',
       main_image: 'https://place-hold.it/600x400.jpg/888/fff/',
       thumb_image: 'https://place-hold.it/300x200.jpg/888/fff/'
   )
