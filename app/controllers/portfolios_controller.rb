37eddc7589b87b8cd756d10c22265a4a5abe4824
Alex Vlasov <drumlife182@gmail.com>
2022-06-11 12:08:56 +0200

    Integrated database relationship between topics and blog posts

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -14,11 +14,11 @@ class PortfoliosController < ApplicationController
   def edit; end
 
   def create
-    @portfolio_item = Portfolio.new(portfolios_params)
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live' }
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
       end
@@ -27,7 +27,7 @@ class PortfoliosController < ApplicationController
 
   def update
     respond_to do |format|
-      if @portfolio_item.update(portfolios_params)
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Portfolio item is updated' }
       else
         format.html { render :edit }
@@ -50,7 +50,7 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
   end
 
-  def portfolios_params
+  def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body)
   end
 
