29406a419e3836ffbe55298736df436aa43a8958
Alex Vlasov <drumlife182@gmail.com>
2022-06-16 08:03:32 +0200

    Added nav bar and added default header content

---
+++ app/controllers/concerns/current_user_concern.rb
@@ -0,0 +1,15 @@
+module CurrentUserConcern
+  extend ActiveSupport::Concern
+
+  included do
+    before_filter :current_user
+  end
+
+  def current_user
+    super || guest_user
+  end
+
+  def guest_user
+    OpenStruct.new(name: 'Guest User', first_name: 'Guest', last_name: 'User', email: 'guest@user.com')
+  end
+end
---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,12 @@
+module DefaultPageContent
+  extend ActiveSupport::Concern
+
+  included do
+    before_filter :set_page_defaults
+  end
+
+  def set_page_defaults
+    @page_title = "Alex Vlasov | My Portfolio"
+    @seo_keywords = "Alex Vlasov portfolio"
+  end
+end
