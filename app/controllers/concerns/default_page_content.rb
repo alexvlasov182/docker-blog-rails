7b3d84574bdc762c20938d9075a58446409b02e4
Alex Vlasov <drumlife182@gmail.com>
2022-06-20 20:49:04 +0200

    Changed some style for concern controller

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
   end
 
   def set_page_defaults
-    @page_title = "Alex Vlasov | My Portfolio"
-    @seo_keywords = "Alex Vlasov portfolio"
+    @page_title = 'Alex Vlasov | My Portfolio'
+    @seo_keywords = 'Alex Vlasov portfolio'
   end
 end
