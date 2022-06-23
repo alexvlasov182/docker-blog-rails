eaab906eab1de5f90e9eda29e272699a4446fe2b
Alex Vlasov <drumlife182@gmail.com>
2022-06-23 09:43:00 +0200

    Updated master layout and blog layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ end
 
 10.times do |blog|
   Blog.create!(
-    title: "My Blog Post About Ruby on Rails #{blog}",
+    title: "My Blog Post About UX Design #{blog}",
     body: "RubyGems.org is the Ruby community’s gem hosting service. Instantly publish your gems and then install them. Use the API to find out more about available gems. Become a contributor and improve the site yourself.
 RubyGems.org is made possible through a partnership with the greater Ruby community. Fastly provides bandwidth and CDN support, Ruby Central covers infrastructure costs, and Ruby Together funds ongoing development and ops work. Learn more about our sponsors and how they work together.
 We need your help to fund the developer time that keeps RubyGems.org running smoothly for everyone. Join Ruby Together today.
@@ -19,7 +19,7 @@ puts '10 blog posts created'
 
 5.times do |skill|
   Skill.create!(
-    title: "Rails #{skill}",
+    title: "UX Design #{skill}",
     percent_utilized: 15
   )
 end
@@ -29,20 +29,10 @@ puts '5 skills created'
 8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title #{portfolio_item}",
-    subtitle: 'Ruby on Rails',
-    body: 'RubyGems.org is made possible through a partnership with the greater Ruby community. Fastly provides bandwidth and CDN support, Ruby Central covers infrastructure costs, and Ruby Together funds ongoing development and ops work. Learn more about our sponsors and how they work together.',
+    subtitle: 'UX Design',
+    body: 'Hands-on exercises, real-world scenarios, and practical team-based decision-making tools will get everyone on board and give you the experience you need to become a confident software architect. With dozens of design methods, examples, and practical know-how, Design It! shows you how to become a software architect. Walk through the core concepts every architect must know, discover how to apply them, and learn a variety of skills that will make you a better programmer, leader, and designer.',
     main_image: 'https://place-hold.it/600x400.jpg/888/fff/',
     thumb_image: 'https://place-hold.it/300x200.jpg/888/fff/'
-  )
-end
-
-1.times do |portfolio_item|
-  Portfolio.create!(
-      title: "Portfolio title #{portfolio_item}",
-      subtitle: 'Angular',
-      body: 'Build features quickly with simple, declarative templates. Extend the template language with your own components and use a wide array of existing components. Get immediate Angular-specific help and feedback with nearly every IDE and editor. All this comes together so you can focus on building amazing apps rather than trying to make the code work.',
-      main_image: 'https://place-hold.it/600x400.jpg/888/fff/',
-      thumb_image: 'https://place-hold.it/300x200.jpg/888/fff/'
   )
 end
 
