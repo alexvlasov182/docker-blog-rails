fe5ff4731102f177057dab5b52910f443bd71b54
Alex Vlasov <drumlife182@gmail.com>
2022-06-27 13:27:30 +0200

    Implemented action cable for comments

--- config/application.rb
+++ config/application.rb
@@ -19,11 +19,7 @@ Bundler.require(*Rails.groups)
 module Myapp
   class Application < Rails::Application
     config.eager_load_paths << "#{Rails.root}/lib"
-    # Initialize configuration defaults for originally generated Rails version.
-    #config.load_defaults 5.1
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.action_controller.permit_all_parameters = true
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
